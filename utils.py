0debbcc91ef87696dfd11b61eaa5c1c9984cf0a6
Diego Gomez Mosquera <diegoalejogm@gmail.com>
2018-01-20 00:22:45 -0500

    Updated notebook

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import numpy as np
 from torch.autograd import Variable
+from torchvision import transforms
 import torch
 
 IMAGES_PATH = './images'
@@ -24,40 +25,44 @@ def load_image(path, max_size=None, shape=None, display=True):
     if shape is not None:
         img = img.resize(shape, Image.ANTIALIAS)
 
-    # Convert image to numpy array
-    arr = np.array(img)
+    # Transform image to tensor
+    tensor = transforms.ToTensor()(img)
 
     # Display image
     if display:
-        display_image(arr)
+        display_image(tensor)
 
-    # Return numpy array
-    return arr
+    # Return tensor
+    return tensor
 
 
-def image_to_var(arr):
+def tensor_to_4d_var(tensor, requires_grad=False, cuda=False):
     '''
-    Convert 3D numpy image to 4D PyTorch Variable
+    Return image tensor with additional dimension N, as Variable
     '''
-    # Reformat array WHC to CWH
-    arr = np.moveaxis(arr, 2, 0)
-    # Convert to float Tensor
-    tensor = torch.from_numpy(arr).float()
-    # Reformat add 4th dimension N: CWH to NCWH
-    tensor = tensor.unsqueeze(0)
-    # Convert to Variable
-    return Variable(tensor)
+    var = Variable(tensor.unsqueeze(0), requires_grad=requires_grad)
+
+    if cuda:
+        var = var.cuda()
+    return var
 
 
-def display_image(arr, format='HWC'):
+def display_image(tensor, format='HWC'):
     '''
-    Displays image from numpy array using MatplotLib
+    Displays image from tensor using MatplotLib
     '''
-    if format == 'CHW':
-        arr = np.moveaxis(arr, 0, -1)
+    # Reset plot
     plt.close()
     plt.figure()
-    plt.imshow(arr)
+
+    if (len(tensor.size()) > 3):
+        # Remove the batch dimension
+        tensor = tensor.squeeze(0)
+    # Load image
+    img = transforms.ToPILImage()(tensor.clone().cpu())
+    # Show image
+    plt.imshow(img)
+    plt.show()
 
 
 def gram_matrices(list):
@@ -74,42 +79,43 @@ def _gram_matrix(tensor):
     '''
     Computes the gram matrix of a 4-D (NCWH) Convolved Tensor.
     '''
-    # Here N is num_tensors, no num_feature_maps as in the paper
-    N = tensor.size(0)
-    # Here C is num_feature_maps
-    C = tensor.size(1)
+    # Here M is minibatch size
+    # Here N is num_feature_maps
+    M, N, W, H = tensor.size()
     # Merge height and width dimensions into M: new shape: NCM
-    tensor = tensor.view(N * C, -1)
+    tensor = tensor.view(M * N, W * H)
     # Calculate transposed tensor
     tensor_T = tensor.transpose(0, 1)
     # Calculate Gram matrix as tensor_T * tensor
     tensor = torch.mm(tensor, tensor_T)
-    return tensor
+    # print(C * W * H)
+    return tensor / (4 * N**2 * (W * H)**2)
 
 
-def style_loss(criterion, target_style, noise_style):
+def style_loss(criterion, target_style, input_style):
     '''
     Calculates the style loss given a loss criterion
     '''
     loss = 0
-    num_layers = len(target_style)
-    w = 1. / num_layers
+    layer_weight = 1. / len(target_style)
     for i, _ in enumerate(target_style):
+        # print(i)
         style_i = target_style[i].detach()
-        noise_i = noise_style[i]
-        loss_i = criterion(noise_i, style_i) / w
+        input_i = input_style[i]
+        loss_i = criterion(input_i, style_i) * layer_weight
         loss += loss_i
     return loss
 
 
-def content_loss(criterion, target_content, noise_content):
+def content_loss(criterion, target_content, input_content):
     '''
     Calculates the content loss given a loss criterion
     '''
     loss = 0
     for i, _ in enumerate(target_content):
+        # print(i)
         content_i = target_content[i].detach()
-        noise_i = noise_content[i]
-        loss_i = criterion(noise_i, content_i)
+        input_i = input_content[i]
+        loss_i = criterion(input_i, content_i)
         loss += loss_i
     return loss
